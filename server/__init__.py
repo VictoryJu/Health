d51bf00deb01deed22015f5fb6c2ffc2fd7d5fbe
VictoryJu <kob0805@naver.com>
2021-04-19 22:47:02 +0900

    add: router guide

--- server/__init__.py
+++ server/__init__.py
@@ -8,30 +8,35 @@ app = Flask(__name__)
 app.debug = True
 
 #person 테이블 관련
+# 전체환자수 조회
 @app.route("/person",methods=['GET'])
 def getAllPerson():
   result = model.getAllPerson()
   print(result)
   return str(result)
 
+# 성별 환자수 조회
 @app.route("/person/gender",methods=['GET'])
 def getAllGender():
   result = model.getAllGender()
   print(result)
   return str(result)
 
+# 인종별 환자수 조회
 @app.route("/person/race",methods=['GET'])
 def getRaceConcept():
   result = model.getRaceConcept()
   print(result)
   return str(result)
 
+# 민족별 환자수 조회
 @app.route("/person/ethnicity",methods=['GET'])
 def getAllEthnicity():
   result = model.getAllEthnicity()
   print(result)
   return str(result)
 
+#사망 환자수 조회
 @app.route("/person/death",methods=['GET'])
 def getDeathPerson():
   result = model.getDeathPerson()
@@ -39,6 +44,7 @@ def getDeathPerson():
   return str(result)
 
 # 키워드 검색
+# concept_id 정보 조회
 @app.route("/serch/<int:serchIdx>",methods=['GET'])
 def getSerchConcept(serchIdx):
   result = model.getSerchConcept(serchIdx)
@@ -46,6 +52,7 @@ def getSerchConcept(serchIdx):
   return str(result)
 
 # visit 테이블 관련
+# 성별 방문수 조회
 @app.route("/visit/gender",methods=['GET'])
 def getGenderVisit():
   result = model.getGenderVisit()
